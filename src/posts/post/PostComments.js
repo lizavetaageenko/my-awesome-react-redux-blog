0e436d0d39dc8b36112f5d0464a9901705e86f80
Yelyzaveta Buts <lizavetaageenko@gmail.com>
2017-02-08 17:54:01 +0200

    Add comments

--- src/posts/post/PostComments.js
+++ src/posts/post/PostComments.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
-const PostComments = () => (
+const PostComments = ({ comments }) => (
     <div>
+        {
+            comments.map((comment) => (
+                <div>
+                    <h4>{comment.name}</h4>
+                    <div>{comment.body}</div>
+                </div>
+            ))
+        }
     </div>
 );
 
 export default PostComments;
+
+PostComments.propTypes = {
+    comments: React.PropTypes.array
+};
+
+PostComments.defaultProps = {
+    comments: []
+};
--- src/posts/postsReducer.js
+++ src/posts/postsReducer.js
@@ -3,7 +3,16 @@ import { UPDATE_POSTS } from './postsActions';
 export default function (state = [], action) {
     switch (action.type) {
         case UPDATE_POSTS: {
-            return action.posts.slice();
+            return action.posts
+                .slice(0, 10)
+                .map(({ id, title, body }) => ({
+                    id,
+                    title,
+                    body,
+                    isEditable: false,
+                    isCommentsShown: false,
+                    comments: []
+                }));
         }
 
         default: {
